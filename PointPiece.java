007651a4772f6c3b519e686c4d3be5922a9a0744
MohamedImran7868 <108204134+MohamedImran7868@users.noreply.github.com>
2024-01-26 09:00:41 +0800

    Swap update

--- PointPiece.java
+++ PointPiece.java
@@ -2,17 +2,19 @@ import java.io.File;
 
 public class PointPiece {
     public String name;
-    private File imagePath;
+    public File imagePath;
     public int xCoordinate;
     public int yCoordinate;
     public int player;
+    public String status;
 
-    public PointPiece(String name, File file, int x, int y, int player) {
+    public PointPiece(String name, File file, int x, int y, int player, String status) {
         this.name = name;
         this.imagePath = file;
         this.xCoordinate = x;
         this.yCoordinate = y;
         this.player = player;
+        this.status = status;
     }
 
     public String getName() {
@@ -33,5 +35,17 @@ public class PointPiece {
 
     public int getPlayer() {
         return player;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setImagePath(File imagePath) {
+        this.imagePath = imagePath;
     }
 }
